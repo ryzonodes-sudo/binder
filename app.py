01b90d8b20b9361d63ecd0c8a3538efa236ed4c7
ryzonodes-sudo <ryzonodes@gmail.com>
2026-05-24 17:07:30 +0530

    Create app.py

---
+++ app.py
@@ -0,0 +1,10 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return "Ubuntu 24 Binder Working"
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=7860)
